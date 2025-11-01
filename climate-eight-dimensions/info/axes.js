2f4ee9a26bb8cd5bdde2d12165107f8e74983e76
aoikuma <48854115+BluebearOfficial@users.noreply.github.com>
2025-11-01 21:50:00 +0800

    Add files via upload

---
+++ climate-eight-dimensions/info/axes.js
@@ -0,0 +1,66 @@
+axes = [
+    {
+        "id": "class",
+        "name": "气温高低",
+        "left": "严寒赛高",
+        "right": "热不难受",
+        "leftEmoji": "🐕",
+        "rightEmoji": "👑"
+    },
+    {
+        "id": "gender",
+        "name": "昼夜阴晴",
+        "left": "阴凉舒爽",
+        "right": "放晴开心",
+        "leftEmoji": "💊",
+        "rightEmoji": "✊"
+    },
+    {
+        "id": "life",
+        "name": "干燥湿润",
+        "left": "湿润清新",
+        "right": "干爽舒适",
+        "leftEmoji": "🛌",
+        "rightEmoji": "👨‍💻"
+    },
+    {
+        "id": "wealth",
+        "name": "温差大小",
+        "left": "恒温最好",
+        "right": "晨袄午纱",
+        "leftEmoji": "🔪",
+        "rightEmoji": "🐺"
+    },
+    {
+        "id": "auth",
+        "name": "选地因素",
+        "left": "气候第一",
+        "right": "综合考虑",
+        "leftEmoji": "🕊️",
+        "rightEmoji": "👁️"
+    },
+    {
+        "id": "cult",
+        "name": "换季态度",
+        "left": "均温重要",
+        "right": "夏短冬长",
+        "leftEmoji": "🗽",
+        "rightEmoji": "🐲"
+    },
+    {
+        "id": "group",
+        "name": "天气过程",
+        "left": "喜欢平和",
+        "right": "盼望台风",
+        "leftEmoji": "💰",
+        "rightEmoji": "⚙️"
+    },
+    {
+        "id": "world",
+        "name": "你是什么",
+        "left": "爱好天气",
+        "right": "凡夫俗子",
+        "leftEmoji": "🌐",
+        "rightEmoji": "🚧"
+    }
+]; 
